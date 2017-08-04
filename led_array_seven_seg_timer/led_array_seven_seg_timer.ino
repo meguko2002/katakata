72ab580938d832ea702733291ff268408de9f433
poko-PC\poko <meguko2002@gmail.com>
2017-08-04 22:42:50 +0900

    コメント削除

--- led_array_seven_seg_timer/led_array_seven_seg_timer.ino
+++ led_array_seven_seg_timer/led_array_seven_seg_timer.ino
@@ -40,13 +40,7 @@ int state = 0;
 #define MAX_VAL 8  // 0 to 255 for brightness
 #define DELAY_TIME1 100 // 待機時遅延時間
 #define DELAY_TIME2 25 // ゴール時遅延時間
-// Parameter 1 = number of pixels in strip
-// Parameter 2 = pin number (most are valid)
-// Parameter 3 = pixel type flags, add together as needed:
-//   NEO_RGB     Pixels are wired for RGB bitstream
-//   NEO_GRB     Pixels are wired for GRB bitstream
-//   NEO_KHZ400  400 KHz bitstream (e.g. FLORA pixels)
-//   NEO_KHZ800  800 KHz bitstream (e.g. High Density LED strip)
+
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(20, LED_PIN, NEO_GRB + NEO_KHZ800);
 uint16_t picnum = 0;
 uint16_t colval = 0;
